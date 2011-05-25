e7a0ef8e8daa049f7ac39e0f9928edd13fcb9c5e
Trajche Petrov <skechboy@gmail.com>
2011-05-25 19:11:47 +0200

    Version 0.2.2

--- js/content_script.js
+++
@@ -1,7 +0,0 @@
-/*
- * -----------------------------------------------------------------------------
- *  Get selected text don't work on iframes
- * -----------------------------------------------------------------------------
-*/	
-var selected = {"text": window.getSelection().toString()};
-chrome.extension.sendRequest(selected); // Sendback the selected text
---
+++ js/get_selection.js
@@ -0,0 +1,18 @@
+/*
+ * SpeakIt GUI
+ *
+ * This file contains code that displays Grafical User Interface
+ *
+ * @package		SpeakIt
+ * @category	GUI
+ * @author		Trajche Petrov a.k.a SkechBoy
+ * @link		https://github.com/skechboy/SpeakIt
+ 
+ * -----------------------------------------------------------------------------
+ *  Get selected text don't work on iframes and some ssl encrypted pages
+ *  depends' on level of encryption of page certificate...
+ *  Sorry for that but again is Google's fault, damn Chrome is the safest
+ *  browser ever build :) 
+ * -----------------------------------------------------------------------------
+*/	
+chrome.extension.sendRequest({"text": window.getSelection().toString()});
